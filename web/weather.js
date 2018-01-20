f7a49a120674349fa34cf1a0960bd1d06bc84b8d
Sterling Finn <sefinn@edu.uwaterloo.ca>
2018-01-20 05:29:25 -0500

    weather and maps update

---
+++ web/weather.js
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+
+    <head>
+
+
+
+
+    </head>
+
+
+    <body>
+
+
+        <!--weather widget start--><a target="_blank" href="http://www.booked.net/weather/lafayette-hill-17499"><img src="https://w.bookcdn.com/weather/picture/2_17499,18114,18103_1_1_137AE9_250_ffffff_333333_08488D_1_ffffff_333333_0_6.png?scode=124&domid=w209&anc_id=95648"
+            alt="booked.net"/></a><!-- weather widget end -->
+
+         
+
+
+    </body>
+
+
+</html>
