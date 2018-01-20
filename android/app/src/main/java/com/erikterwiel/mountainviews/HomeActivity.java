721c3eb40b6cada3a2e5b27fe14ea28690dba839
ehterwie <ehterwie@uwaterloo.ca>
2018-01-20 00:53:00 -0500

    Tabbed toolbar now works

--- android/app/src/main/java/com/erikterwiel/mountainviews/HomeActivity.java
+++ android/app/src/main/java/com/erikterwiel/mountainviews/HomeActivity.java
@@ -1,13 +1,35 @@
 package com.erikterwiel.mountainviews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "HomeActivity.java";
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i(TAG, "onCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//        getSupportActionBar().hide();
+
+        mViewPager = (ViewPager) findViewById(R.id.home_viewpager);
+        mTabLayout = (TabLayout) findViewById(R.id.home_tabs);
+
+        HomePagesAdapter adapter = new HomePagesAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FeedFragment(), "Feed");
+        adapter.addFragment(new ProfileFragment(), "Profile");
+        mViewPager.setAdapter(adapter);
+
+        mTabLayout.setupWithViewPager(mViewPager);
     }
+
 }
