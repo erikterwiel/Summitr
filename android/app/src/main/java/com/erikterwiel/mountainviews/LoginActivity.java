f45fc468adfb671a67f528e2e7116d8be9bc7e5d
ehterwie <ehterwie@uwaterloo.ca>
2018-01-19 21:26:53 -0500

    Layout for RegisterActivity done

--- android/app/src/main/java/com/erikterwiel/mountainviews/LoginActivity.java
+++ android/app/src/main/java/com/erikterwiel/mountainviews/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.erikterwiel.mountainviews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity.java";
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mLogin;
+    private Button mRegister;
+    private Button mForgot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i(TAG, "onCreate() called");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mRegister = (Button) findViewById(R.id.login_up);
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
