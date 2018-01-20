528c8b3d2821047265bd7e1cc660e2b0c159ae44
ehterwie <ehterwie@uwaterloo.ca>
2018-01-20 01:18:40 -0500

    All 5 fragments added to HomeActivity

---
+++ android/app/src/main/java/com/erikterwiel/mountainviews/TrackerFragment.java
@@ -0,0 +1,21 @@
+package com.erikterwiel.mountainviews;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TrackerFragment extends Fragment {
+
+    public static final String TAG = "TrackerFragment.java";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        return view;
+    }
+}
