043631244a98b9de38af25b92e69530cc52b6477
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-01-21 03:49:11 -0500

    split posts into photos and reborts

--- web/nltk.py
+++ web/nltk.py
@@ -2,17 +2,39 @@ import boto3
 import bs4
 
 dynamodb = boto3.resource('dynamodb')
+s3 = boto3.resource('s3')
 
+recents = dynamodb.Table('recents')
 table = dynamodb.Table('reports')
+
+time = int(input())
+
+info = recents.get_item(
+    Key={
+        'time': time,
+    }
+)
+
+response = table.get_item(
+    Key={
+        'title': txt,
+    }
+)
+
+identifier = info['Item']['identifier']
+post_type = info['Item']['type']
+report_item = response['Item']['report']
+
 client = boto3.client('comprehend',region_name='us-west-2')
 
-def sentimenter(txt):
-    response = table.get_item(
-        Key={
-            'title': txt,
-        }
-    )
+def load_pictures(txt):
+    for key in bucket.objects.all():
+        print(key.key)
+    #obj = s3.Object('mountainviews', 'key')
+    #file_contents = obj.get()["Body"].read()
+    #print file_contents.count('\n')
 
+def sentimenter(id):
     with open("index.html") as inf:
         txt = inf.read()
         soup = bs4.BeautifulSoup(txt)
@@ -23,6 +45,9 @@ def sentimenter(txt):
     new_title = soup.new_tag('h2')
     new_title.insert(0,response['Item']['title'])
     new_tag.append(new_title)
+    
+    for key in bucket.objects.all():
+        print(key.key)
 
     new_pic = soup.new_tag('img',src='githubprof.png')
     new_tag.append(new_pic)
@@ -35,14 +60,12 @@ def sentimenter(txt):
     with open("index.html", "w") as outf:
         outf.write(str(soup))
 
-    report_item = response['Item']['report']
-
-    response = client.detect_sentiment(
+    sent = client.detect_sentiment(
         Text=report_item,
         LanguageCode='en'
     )
 
-    star_count = int(response['SentimentScore']['Positive']*5)
+    star_count = int(sent['SentimentScore']['Positive']*5)
 
     table.update_item(
         Key={
@@ -53,6 +76,45 @@ def sentimenter(txt):
             ':val1': star_count
         }
     )
-to_analyze = str(input())
 
-sentimenter(to_analyze)
+
+def display_photos(id):
+    for key in bucket.objects.all():
+        print(key.key)
+    #obj = s3.Object('mountainviews', 'key')
+    #file_contents = obj.get()["Body"].read()
+    #print file_contents.count('\n')
+
+def display_photos(id):
+    with open("index.html") as inf:
+        txt = inf.read()
+        soup = bs4.BeautifulSoup(txt)
+
+    new_tag = soup.new_tag('div class="posts"')
+    soup.body.insert(4, new_tag)
+
+    new_title = soup.new_tag('h2')
+    new_title.insert(0,response['Item']['title'])
+    new_tag.append(new_title)
+    
+    for key in bucket.objects.all():
+        print(key.key)
+
+    new_pic = soup.new_tag('img',src='githubprof.png')
+    new_tag.append(new_pic)
+
+    new_report = soup.new_tag('p')
+    report_item = response['Item']['report']
+    new_report.insert(0, report_item)
+    new_tag.append(new_report)
+
+    with open("index.html", "w") as outf:
+        outf.write(str(soup))
+
+if post_type == 'report':
+    sentimenter(identifier)
+elif post_type == 'photo':
+    display_photos(identifier)
+else:
+    print('Invalid post type.')
+
