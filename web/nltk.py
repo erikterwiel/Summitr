15e262747e8c0c1e5802fb11d0a7dbaf5c740cec
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-01-21 05:11:29 -0500

    all post data being displayed

--- web/nltk.py
+++ web/nltk.py
@@ -6,6 +6,7 @@ s3 = boto3.resource('s3')
 
 recents = dynamodb.Table('recents')
 table = dynamodb.Table('reports')
+pictures = dynamodb.Table('photos')
 
 time = int(input())
 
@@ -15,15 +16,30 @@ info = recents.get_item(
     }
 )
 
-response = table.get_item(
-    Key={
-        'title': txt,
-    }
-)
-
 identifier = info['Item']['identifier']
 post_type = info['Item']['type']
-report_item = response['Item']['report']
+userid = info['Item']['username']
+
+if post_type == 'report':
+    response = table.get_item(
+        Key={
+            'title': identifier,
+        }
+    )
+    report_item = response['Item']['report']
+
+elif post_type == 'photo':
+    response = recents.get_item(
+        Key={
+            'time': time,
+        }
+    )
+    pics = pictures.get_item(
+        Key={
+            'filename': identifier,
+        }
+    )
+    caption = pics['Item']['caption']
 
 client = boto3.client('comprehend',region_name='us-west-2')
 
@@ -45,15 +61,19 @@ def sentimenter(id):
     new_title = soup.new_tag('h2')
     new_title.insert(0,response['Item']['title'])
     new_tag.append(new_title)
-    
-    for key in bucket.objects.all():
-        print(key.key)
+
+    misc = soup.new_tag('p')
+    misc.insert(0, str(response['Item']['date'])+"|"+str(response['Item']['location'])+"|"+str(response['Item']['distance'])+"|"+str(userid))
+    new_tag.append(misc)
+
+    ######
+    #for key in bucket.objects.all():
+        #print(key.key)
 
     new_pic = soup.new_tag('img',src='githubprof.png')
     new_tag.append(new_pic)
 
     new_report = soup.new_tag('p')
-    report_item = response['Item']['report']
     new_report.insert(0, report_item)
     new_tag.append(new_report)
 
@@ -69,7 +89,7 @@ def sentimenter(id):
 
     table.update_item(
         Key={
-            'title': report_item,
+            'title': id,
         },
         UpdateExpression='SET ranking = :val1',
         ExpressionAttributeValues={
@@ -78,12 +98,6 @@ def sentimenter(id):
     )
 
 
-def display_photos(id):
-    for key in bucket.objects.all():
-        print(key.key)
-    #obj = s3.Object('mountainviews', 'key')
-    #file_contents = obj.get()["Body"].read()
-    #print file_contents.count('\n')
 
 def display_photos(id):
     with open("index.html") as inf:
@@ -92,24 +106,39 @@ def display_photos(id):
 
     new_tag = soup.new_tag('div class="posts"')
     soup.body.insert(4, new_tag)
+    ##insert stars
+    #for key in bucket.objects.all():
+        #print(key.key)
+    user_id = soup.new_tag('p')
+    user_id.insert(0, userid)
+    new_tag.append(user_id)
 
-    new_title = soup.new_tag('h2')
-    new_title.insert(0,response['Item']['title'])
-    new_tag.append(new_title)
-    
-    for key in bucket.objects.all():
-        print(key.key)
-
-    new_pic = soup.new_tag('img',src='githubprof.png')
+    new_pic = soup.new_tag('img', src='githubprof.png')
     new_tag.append(new_pic)
-
-    new_report = soup.new_tag('p')
-    report_item = response['Item']['report']
-    new_report.insert(0, report_item)
-    new_tag.append(new_report)
+#####
+    new_caption = soup.new_tag('p')
+    new_caption.insert(0, caption)
+    new_tag.append(new_caption)
 
     with open("index.html", "w") as outf:
         outf.write(str(soup))
+
+    sent = client.detect_sentiment(
+        Text=caption,
+        LanguageCode='en'
+    )
+
+    star_count = int(sent['SentimentScore']['Positive']*5)
+
+    pictures.update_item(
+        Key={
+            'filename': id,
+        },
+        UpdateExpression='SET ranking = :val1',
+        ExpressionAttributeValues={
+            ':val1': star_count
+        }
+    )
 
 if post_type == 'report':
     sentimenter(identifier)
