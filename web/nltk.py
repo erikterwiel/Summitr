1381ba6729dbc96dbf42de0df4f6aa80431a20c3
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-01-20 22:17:16 -0500

    Authentication Error for update dynamodp table

--- web/nltk.py
+++ web/nltk.py
@@ -1,33 +1,32 @@
 import boto3
-
-# Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 
 table = dynamodb.Table('test')
 client = boto3.client('comprehend',region_name='us-west-2')
 
-def sentimenter(post_name):
-    report = table.get_item(
+def sentimenter(txt):
+    response = table.get_item(
         Key={
-            'test1': 'Good morning',
+            'test1': txt,
         }
     )
-    report_item = report['Item']['report']
+
+    report_item = response['Item']['report']
 
     response = client.detect_sentiment(
         Text=report_item,
         LanguageCode='en'
     )
+
     star_count = int(response['SentimentScore']['Positive']*5)
-    print(star_count)
+
     table.update_item(
-    Key={
-        'test1': 'Good morning',
-        'rating': 0
+        Key={
+            'test1': txt,
         },
-    UpdateExpression='SET rating = :val1',
-    ExpressionAttributeValues={
-        ':val1': star_count
+        UpdateExpression='SET rating = :val1',
+        ExpressionAttributeValues={
+            ':val1': star_count
         }
     )
 
